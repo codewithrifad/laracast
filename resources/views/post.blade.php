34f8237361bb45fe5d2ef0e8a69c0a6064d6d7e8
codewithrifad <codewithrifad@gmail.com>
2022-02-18 00:40:40 +0530

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    {{-- <script src="/app.js"></script> --}}
+
+
+
+
+    <title>My Laracast Blog</title>
+</head>
+
+<body>
+    <article>
+
+        <h1 style="text-align: center">Hello Laracast</h1>
+        <hr>
+
+       <?= $post; ?>
+    </article>
+
+  <a href="/"> Go Back</a>
+
+</body>
+
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    {{-- <script src="/app.js"></script> --}}
+
+
+
+
+    <title>My Laracast Blog</title>
+</head>
+
+<body>
+    <article>
+
+        <h1 style="text-align: center">Hello Laracast</h1>
+        <hr>
+
+        <h2> <a href="/posts/my-first-post"> My First Post </a> </h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt? Enim quisquam animi
+            architecto iusto exercitationem optio doloribus repellat voluptas expedita harum et sequi dolorum quia quo
+            totam, ipsa laborum dolor error? Eos voluptate aliquam aspernatur excepturi commodi blanditiis corrupti?</p>
+    </article>
+
+    <article>
+
+        <h2><a href="/posts/my-second-post">My Second Post </a></h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt? Enim quisquam animi
+            architecto iusto exercitationem optio doloribus repellat voluptas expedita harum et sequi dolorum quia quo
+            totam, ipsa laborum dolor error? Eos voluptate aliquam aspernatur excepturi commodi blanditiis corrupti?</p>
+    </article>
+
+    <article>
+
+        <h2><a href="/posts/my-third-post"> My Third Post </a></h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt? Enim quisquam animi
+            architecto iusto exercitationem optio doloribus repellat voluptas expedita harum et sequi dolorum quia quo
+            totam, ipsa laborum dolor error? Eos voluptate aliquam aspernatur excepturi commodi blanditiis corrupti?</p>
+    </article>
+
+</body>
+
+</html>
